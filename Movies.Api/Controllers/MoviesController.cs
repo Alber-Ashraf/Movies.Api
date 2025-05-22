6bbcdf0a0f9461cb4028eb4d13317267e6e5496e
BeRo <BeRo@BERO>
2025-05-22 13:03:47 +0300

    Add Create api for Movies

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+using Movies.Application.Repositories.IRepositories;
+using Movies.Application.Models;
+
+namespace Movies.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [HttpPost("Movies")]
+        public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+        {
+            var movie = new Movie
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                YearOfRelease = request.YearOfRelease,
+                Genres = request.Genres.ToList()
+            };
+            _movieRepository.CreateAsync(movie);
+            return Created($"/api/movies/{movie.Id}", movie);
+        }
+    }
+}
