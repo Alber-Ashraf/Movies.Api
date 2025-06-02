c162796acede7514cce78cd5d03fb05f4a8d7c9c
BeRo <BeRo@BERO>
2025-06-02 11:48:29 +0300

    Integrating auth in Swagger

--- Movies.Api/Swagger/ConfigureSwaggerOptions.cs
+++ Movies.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -1,5 +1,6 @@
 ﻿using Asp.Versioning.ApiExplorer;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Movies.Api.Swagger
@@ -27,6 +28,30 @@ namespace Movies.Api.Swagger
                         Version = description.ApiVersion.ToString(),
                     });
             }
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please provide a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
         }
     }
 }
