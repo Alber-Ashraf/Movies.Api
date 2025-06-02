1f928ce2d1debf8a62221ec33e635d4a2a941db5
BeRo <beroooberooo6@gmail.com>
2025-06-02 21:29:40 +0300

    Instaling Refit package and retrieving a movie

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Movies.Api.Sdk.Consumer
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using Refit;
+
+namespace Movies.Api.Sdk.Consumer
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var moviesApi = RestService.For<IMoviesApi>("https://localhost:7252");
+            var movie = await moviesApi.GetMovieAsync("father-of-the-bride-part-ii2-1995");
+            Console.WriteLine(JsonSerializer.Serialize(movie));
+            Console.ReadKey();
         }
     }
 }
--- Movies.Api.Sdk/ApiEndpoints.cs
+++ Movies.Api.Sdk/ApiEndpoints.cs
@@ -3,7 +3,7 @@ namespace Movies.Api.Sdk
 {
     public static class ApiEndpoints
     {
-        private const string ApiBase = "api";
+        private const string ApiBase = "/api";
         public static class Movies
         {
             private const string Base = $"{ApiBase}/movies";
