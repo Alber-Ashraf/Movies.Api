ed064f9623cbe9f0745b418d448728ae6fcf4a3c
BeRo <beroooberooo6@gmail.com>
2025-06-03 10:04:44 +0300

    Using the HttpClientFactory

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,34 +1,36 @@
 ﻿using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Api.Sdk;
 using Movies.Contracts.Requests;
 using Refit;
 
-namespace Movies.Api.Sdk.Consumer
+//var moviesApi = RestService.For<IMoviesApi>("https://localhost:7252");
+
+var service = new ServiceCollection();
+service.AddRefitClient<IMoviesApi>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7252"));
+
+var serviceProvider = service.BuildServiceProvider();
+var moviesApi = serviceProvider.GetRequiredService<IMoviesApi>();
+
+var movie = await moviesApi.GetMovieAsync("father-of-the-bride-part-ii2-1995");
+//Console.WriteLine(JsonSerializer.Serialize(movie));
+
+var request = new GetAllMoviesRequest
 {
-    internal class Program
-    {
-        static async Task Main(string[] args)
-        {
-            var moviesApi = RestService.For<IMoviesApi>("https://localhost:7252");
-            var movie = await moviesApi.GetMovieAsync("father-of-the-bride-part-ii2-1995");
-            //Console.WriteLine(JsonSerializer.Serialize(movie));
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 3,
+};
 
-            var request = new GetAllMoviesRequest
-            {
-                Title = null,
-                Year = null,
-                SortBy = null,
-                Page = 1,
-                PageSize = 3,
-            };
+var allMovies = await moviesApi.GetAllMoviesAsync(request);
 
-            var allMovies = await moviesApi.GetAllMoviesAsync(request);
-             
-            foreach ( var movieResponse in allMovies.Movies ) {
-                Console.WriteLine(JsonSerializer.Serialize(movieResponse));
-            }
-
-            Console.ReadKey();
-        }
-    }
+foreach (var movieResponse in allMovies.Movies)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
 }
+
+Console.ReadKey();
